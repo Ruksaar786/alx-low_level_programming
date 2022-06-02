dbd56dc99dd05c55b9122baf2bc106e7e2aa0e6a
Ruksaar786 <adamruksaar1@gmail.com>
2022-06-02 15:45:40 -0700

    My commit task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - entry point 
+ * main - entry point
  *My hello, printf task
- *return: return 1 after program executes 
+ *Return: return 1 after program executes
  */
 int main(void)
 {
