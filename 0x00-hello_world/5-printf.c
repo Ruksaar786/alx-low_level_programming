62a9c2a96f87a37147e28adaf636a4a99fecf87b
Ruksaar786 <adamruksaar1@gmail.com>
2022-06-02 05:53:36 -0700

    my commit task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *My hello, printf task
+ *return: return 1 after program executes 
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
