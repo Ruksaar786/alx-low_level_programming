a92f9a270c49879425588ca15990ff9cbf46733c
Ruksaar786 <adamruksaar1@gmail.com>
2022-06-06 13:50:13 -0700

    advance task 1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,9 @@ int number_left;
 int number_right;
 
 
-for (number_left = 48; number_right <= 78; number_left++)
+for (number_left = 48; number_right <= 57; number_left++)
 {
-for (number_right = number_left + 1 ; number_right <= 78; number_right++)
+for (number_right = number_left + 1 ; number_right <= 57; number_right++)
 {
 
 
@@ -21,7 +21,7 @@ putchar(number_left);
 putchar(number_right);
 
 
-if ((number_left == 56) && (number_right == 78))
+if ((number_left == 56) && (number_right == 57))
 {
 break;
 }
