b577083c1107f65adbef3f04c82b11f2cdc5ebee
Ruksaar786 <adamruksaar1@gmail.com>
2022-06-07 15:28:26 -0700

    my commit task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,29 @@
 #include "main.h"
 
 /**
- * main - check the code
+ *  print_last_digit - last digit
  *
+ *@n: The int to print
  * Return: Always 0.
  */
-int main(void)
-{
-    int r;
 
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
-    return (0);
+int print_last_digit(int n)
+
+{
+int last_digit;
+
+if (n < 0)
+{
+last_digit = (-1 * (n % 10));
+_putchar (last_digit + '0');
+return (last_digit);
+}
+
+else
+{
+last_digit = (n % 10);
+_putchar (last_digit + '0');
+return (last_digit);
+}
+
 }
