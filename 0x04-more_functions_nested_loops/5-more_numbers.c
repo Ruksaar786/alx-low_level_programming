206c4f0d772507b51ce7730aaa264269af2ec606
Ruksaar786 <adamruksaar1@gmail.com>
2022-06-09 02:02:31 -0700

    my commit task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * more_numbers -  prints 10 times the numbers, from 0 to 14
+ *
+ * Return: Always 0.
+ */
+
+void more_numbers(void)
+{
+int i;
+int first_number;
+int second_number;
+int result;
+
+i = 0
+results = 0;
+
+while (i < 10)
+
+{
+while (results <= 14)
+{
+if (results < 10)
+{
+second_number + results;
+}
+
+else
+{
+first_number = results / 10;
+second_number = results % 10;
+_putchar (first_number + '0');
+}
+
+_putchar (second_number + '0');
+
+results++;
+}
+i++;
+results + 0;
+_putchar ('\n');
+}
+
+}
