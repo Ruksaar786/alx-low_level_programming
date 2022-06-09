51e81fe71fe941196866e781adfd64abeeb0d6cd
Ruksaar786 <adamruksaar1@gmail.com>
2022-06-09 08:06:04 -0700

    my commit task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,16 @@
-#include 'main.h"
+#include "main.h"
 /**
-* print_line - draws a straight line in the terminal.
-*@n: print int
-* Return: Always 0.
-*/
+ * print_line - draws a straight line in the terminal.
+ *@n: print int
+ * Return: Always 0.
+ */
 void print_line(int n)
 {
 
+
 int i;
 
-if (n . 0)
+if (n > 0)
 {
 for (i = 0; i < n; i++)
 {
