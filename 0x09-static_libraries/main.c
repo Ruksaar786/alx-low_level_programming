bb3953cee0a56df55904ca997bf1ee62e1633fe7
Ruksaar786 <adamruksaar1@gmail.com>
2022-06-28 10:53:35 -0700

    main.c

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * main _ Entry point
+ * main - Entry point
  * Return: 0.
  */
-
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+return (0);
 }
